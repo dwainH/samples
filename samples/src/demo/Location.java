4bef2b82668f9ed617348ae550019f95a2ccf22f
Dwain Hanlon Matin <B032010243@student.utem.edu.my>
2022-03-10 12:53:43 +0800

    Location edit commit

--- samples/src/demo/Location.java
+++ samples/src/demo/Location.java
@@ -2,6 +2,8 @@ package demo;
 
 public class Location {
 	private String address;
+	private String countryName;
+	private String cityName;
 
 	public String getAddress() {
 		return address;
@@ -9,5 +11,21 @@ public class Location {
 
 	public void setAddress(String address) {
 		this.address = address;
+	}
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
 	}
 }
