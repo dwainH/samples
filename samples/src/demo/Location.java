1c1a71c1ede978d4b9a52ece282d4b3be8c028f0
Dwain Hanlon Matin <B032010243@student.utem.edu.my>
2022-03-10 12:50:12 +0800

    Location commit

---
+++ samples/src/demo/Location.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class Location {
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
